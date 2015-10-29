126bbec6424be76d540e7363359b9d49d697d5d7
Renzo <renzors@gmail.com>
2015-10-29 21:29:08 -0200

    Classe foto para teste do JPA

--- src/dominio/Foto.java
+++ src/dominio/Foto.java
@@ -1,7 +1,65 @@
 package dominio;
 
-public class Foto {
+import java.io.Serializable;
 
-	System.out.print();
+public class Foto implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
+	private Integer codFoto;
+	private String caminho;
+	
+	public Foto() {
+	}
+
+	public Foto(Integer codFoto, String caminho) {
+		super();
+		this.codFoto = codFoto;
+		this.caminho = caminho;
+	}
+
+	public Integer getCodFoto() {
+		return codFoto;
+	}
+
+	public void setCodFoto(Integer codFoto) {
+		this.codFoto = codFoto;
+	}
+
+	public String getCaminho() {
+		return caminho;
+	}
+
+	public void setCaminho(String caminho) {
+		this.caminho = caminho;
+	}
+
+	@Override
+	public String toString() {
+		return "Foto [codFoto=" + codFoto + ", caminho=" + caminho + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codFoto == null) ? 0 : codFoto.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Foto other = (Foto) obj;
+		if (codFoto == null) {
+			if (other.codFoto != null)
+				return false;
+		} else if (!codFoto.equals(other.codFoto))
+			return false;
+		return true;
+	}	
 }

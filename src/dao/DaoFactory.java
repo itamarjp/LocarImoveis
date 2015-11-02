410733ce1ac79511ddeb99b9667935b6dce69843
Neirivon Elias Cardoso <neirivon@gmail.com>
2015-11-02 12:23:45 -0200

    DaoFactory Horário Visita

--- src/dao/DaoFactory.java
+++ src/dao/DaoFactory.java
@@ -1,14 +1,20 @@
 package dao;
 
 /**
- * @author Eduardo V. Rocha
+ * @authors Eduardo V. Rocha, Neirivon Elias Cardoso
+ * 
  */
 
 import dao.jpa.AnuncioDaoImpl;
+import dao.jpa.HorarioVisitaDaoImpl;;
 
 public class DaoFactory {
     public static AnuncioDao criarAnuncioDao() {
         return new AnuncioDaoImpl();
     }
+    
+	public static HorarioVisitaDao criarHorarioVisitaDao() {
+		return new HorarioVisitaDaoImpl();
+	}
     
 }
